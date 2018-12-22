e0f9ab9fd81f48e07d4ada4f54585fc6e450a22a
unknown <jodimcarlson4@gmail.com>
2018-12-22 12:59:39 -0600

    did some pseudo coding for my logic still needed

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,14 +1,15 @@
 var survey = require("./friends");
 
 module.exports = function (app) {
-       app.get("/friends", function (request, response) {
-           console.log(survey);
+    app.get("/friends", function (request, response) {
+        console.log(survey);
         response.json(survey);
     });
     app.post("/friends", function (request, response) {
-        //Need for loop to compare all scores to find best match
-
-        friends.push(request.body);
+        //Need for loop to compare all scores to find best match start with 1st person in the array
+        //for (var i = 0; people.length; i++)
+            //people[0].score
+            friends.push(request.body);
     })
 };
 
