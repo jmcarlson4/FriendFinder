ac480e0c16ab03aaa470615531fbe857794c07ba
unknown <jodimcarlson4@gmail.com>
2018-12-22 13:26:06 -0600

    added code

--- apiRoutes.js
+++ apiRoutes.js
@@ -7,9 +7,10 @@ module.exports = function (app) {
     });
     app.post("/friends", function (request, response) {
         //Need for loop to compare all scores to find best match start with 1st person in the array
-        //for (var i = 0; people.length; i++)
-            //people[0].score
-            friends.push(request.body);
+        //for (var i = 0; i< people.length; i++){
+        //people[0].score
+        //}
+        friends.push(request.body);
     })
 };
 
